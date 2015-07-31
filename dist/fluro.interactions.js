5ee2e6d726edf394f6a0ebf13787d98144bbceb8
Cade <cade@hyperblaster.org>
2015-07-31 11:19:57 +1000

    Removed console logs

--- dist/fluro.interactions.js
+++ dist/fluro.interactions.js
@@ -5,144 +5,6 @@ angular.module('fluro.interactions', [
 	'fluro.content',
 ]);
 angular.module('fluro.interactions')
-
-
-.service('FluroInteraction', function(Fluro, FluroContent) {
-
-    var controller = {};
-    
-    //////////////////////////////////////////////////
-
-    controller.interact = function(title, key, interactionData) {
-
-
-        /////////////////////////////////////////////
-
-        //Create a submission to send to our interaction endpoint
-        var submission = {};
-
-        /////////////////////////////////////////////
-
-        //Add the title
-        submission.title = title;
-
-        /////////////////////////////////////////////
-
-        //ID the definition used to translate this interaction
-        submission.key = key;
-
-        /////////////////////////////////////////////
-
-        /**
-         * _contact
-         * _contact can be provided as a full object or an id of an existing contact
-         */
-        if(interactionData._contact) {
-            submission.contact = interactionData._contact;
-        } else {
-            submission.contact = {};
-        }
-
-        /////////////////////////////////////////////
-        /////////////////////////////////////////////
-        /////////////////////////////////////////////
-        
-        //First Name
-        if(interactionData._firstName) {
-            submission.contact.firstName = interactionData._firstName;
-        }
-
-        //Last Name
-        if(interactionData._lastName) {
-            submission.contact.lastName = interactionData._lastName;
-        }
-
-        //Email Address
-        if(interactionData._email) {
-            submission.contact.emails = [interactionData._email];
-        }
-
-        //Email Address (Multiple)
-        if(interactionData._emails) {
-            submission.contact.emails = interactionData._emails;
-        }
-
-        //Phone Number
-        if(interactionData._phoneNumber) {
-            submission.contact.phoneNumbers = [interactionData._phoneNumber];
-        }
-
-        //Phone Number (Multiple)
-        if(interactionData._phoneNumbers) {
-            submission.contact.phoneNumbers = interactionData._phoneNumbers;
-        }
-
-        //Date of birth
-        if(interactionData._dob) {
-            submission.contact.dob = interactionData._dob;
-        }
-
-        //Gender
-        if(interactionData._gender) {
-            submission.contact.gender = interactionData._gender;
-        }
-
-        /////////////////////////////////////////////
-        /////////////////////////////////////////////
-
-        //And include the interaction data
-        submission.interaction = interactionData;
-
-        /////////////////////////////////////////////
-        
-        console.log('Submit interaction', title, key, submission)
-
-        /////////////////////////////////////////////
-
-        //Return the promise
-        return FluroContent.endpoint('interact/' + key).save(submission).$promise;
-    }
-
-    //////////////////////////////////////////////////
-
-    return controller;
-});
-/*
-.service('FluroInteractionService', function(Fluro, FluroContent) {
-
-    //////////////////////////////////////////////////
-
-    var controller = {}
-
-    //////////////////////////
-
-    //Submit and send back the user
-    controller.createInteraction = function(title, contact, definitionKey, submission) {
-        console.log('Submit interaction', definitionKey, contact, submission)
-
-        var interaction = {}
-        interaction.title = title;
-        if(contact) {
-            interaction.contact = contact;
-        }
-        interaction.data = submission;
-        interaction.key =
-        interaction.definition = definitionKey;
-
-        //Save
-        return FluroContent.resource(definitionKey).save(interaction).$promise;
-    }
-
-
-    //////////////////////////////////////////////////
-
-    return controller;
-
-
-});
-
-*/
-angular.module('fluro.interactions')
 .service('FluroValidate', function() {
 
     var controller = {};
@@ -150,6 +12,8 @@ angular.module('fluro.interactions')
     /////////////////////////////////////////////////////
 
     controller.validate = function(entry, field) {
+
+      //  console.log('VALIDATE', entry, field)
 
         //Required
         if (field.minimum > 0 && !entry) {
@@ -162,8 +26,6 @@ angular.module('fluro.interactions')
 
             //Run tests on array
             var array = entry;
-
-
 
             if (field.minimum == 1 && field.maximum == 1 && field.type != 'array') {
                 //console.log(field.key, 'array provided when a single value was expected')
@@ -293,3 +155,137 @@ angular.module('fluro.interactions')
 
 
 });
+angular.module('fluro.interactions')
+
+
+.service('FluroInteraction', function(Fluro, FluroContent) {
+
+    var controller = {};
+    
+    //////////////////////////////////////////////////
+
+    controller.interact = function(title, key, interactionData) {
+
+
+        /////////////////////////////////////////////
+
+        //Create a submission to send to our interaction endpoint
+        var submission = {};
+
+        /////////////////////////////////////////////
+
+        //Add the title
+        submission.title = title;
+
+        /////////////////////////////////////////////
+
+        //ID the definition used to translate this interaction
+        submission.key = key;
+
+        /////////////////////////////////////////////
+
+        /**
+         * _contact
+         * _contact can be provided as a full object or an id of an existing contact
+         */
+        if(interactionData._contact) {
+            submission.contact = interactionData._contact;
+        } else {
+            submission.contact = {};
+        }
+
+        /////////////////////////////////////////////
+        /////////////////////////////////////////////
+        /////////////////////////////////////////////
+        
+        //First Name
+        if(interactionData._firstName) {
+            submission.contact.firstName = interactionData._firstName;
+        }
+
+        //Last Name
+        if(interactionData._lastName) {
+            submission.contact.lastName = interactionData._lastName;
+        }
+
+        //Email Address
+        if(interactionData._email) {
+            submission.contact.emails = [interactionData._email];
+        }
+
+        //Email Address (Multiple)
+        if(interactionData._emails) {
+            submission.contact.emails = interactionData._emails;
+        }
+
+        //Phone Number
+        if(interactionData._phoneNumber) {
+            submission.contact.phoneNumbers = [interactionData._phoneNumber];
+        }
+
+        //Phone Number (Multiple)
+        if(interactionData._phoneNumbers) {
+            submission.contact.phoneNumbers = interactionData._phoneNumbers;
+        }
+
+        //Date of birth
+        if(interactionData._dob) {
+            submission.contact.dob = interactionData._dob;
+        }
+
+        //Gender
+        if(interactionData._gender) {
+            submission.contact.gender = interactionData._gender;
+        }
+
+        /////////////////////////////////////////////
+        /////////////////////////////////////////////
+
+        //And include the interaction data
+        submission.interaction = interactionData;
+        
+        /////////////////////////////////////////////
+
+        //Return the promise
+        return FluroContent.endpoint('interact/' + key).save(submission).$promise;
+    }
+
+    //////////////////////////////////////////////////
+
+    return controller;
+});
+/*
+.service('FluroInteractionService', function(Fluro, FluroContent) {
+
+    //////////////////////////////////////////////////
+
+    var controller = {}
+
+    //////////////////////////
+
+    //Submit and send back the user
+    controller.createInteraction = function(title, contact, definitionKey, submission) {
+        console.log('Submit interaction', definitionKey, contact, submission)
+
+        var interaction = {}
+        interaction.title = title;
+        if(contact) {
+            interaction.contact = contact;
+        }
+        interaction.data = submission;
+        interaction.key =
+        interaction.definition = definitionKey;
+
+        //Save
+        return FluroContent.resource(definitionKey).save(interaction).$promise;
+    }
+
+
+    //////////////////////////////////////////////////
+
+    return controller;
+
+
+});
+
+*/
